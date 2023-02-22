615e9cc7a758d6202369c082395806b6c967bd69
marloupepito <marloupepito07@gmail.com>
2023-02-22 23:25:09 +0800

    dalkwdj lawjdlkajwdlkwaljdwaoiuduioawda

--- resources/js/components/home/components/Parallax.vue
+++ resources/js/components/home/components/Parallax.vue
@@ -1,4 +1,30 @@
 <template>
+  <div>
+  <div class="text-center">
+    <v-bottom-sheet
+      v-model="sheet"
+      persistent
+    >
+  
+      <v-sheet
+        class="text-center"
+        height="100vh"
+         :fullscreen="true"
+      >
+        <v-btn
+          class="mt-6"
+          text
+          color="error"
+          @click="sheet = !sheet"
+        >
+          close
+        </v-btn>
+        <div class="py-3">
+          This is a bottom sheet using the persistent prop
+        </div>
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
     <div
     style="height:100vh; background-size: cover; background-image: url('/images/home.png');background-position: center;"
      
@@ -15,7 +41,7 @@
           <h4 class="subheading" 
         style="padding-top:350px">
             <v-btn
-          class="  mb-3 text-white font-weight-"
+          class="  mb-1 text-white font-weight-"
           @click="searchMap"
           color="black"
           large
@@ -25,12 +51,30 @@
         </v-btn>
           </h4>
         </v-col>
+          <v-col
+          class="text-center"
+          cols="12"
+        >
+   <v-btn
+          class="  mb-3 text-white font-weight-"
+          @click="sheet = true"
+          color="black"
+          large
+          outlined
+        >
+         <b>More Info</b>
+        </v-btn>
+        </v-col>
       </v-row>
+    </div>
     </div>
   </template>
 
 <script>
 export default {
+  data: () => ({
+      sheet: false,
+    }),
     methods:{
         searchMap(){
             this.$router.push({path:'/visit/timeline'})
